f582b1dc639dd565eba305c1f8b28ed5a56651f7
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-17 19:58:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-checkMessageButton.addEventListener("click", () => {
 
+
+checkMessageButton.addEventListener("click", () => {
+  if (messageInput.value === "") {
+    alert("Please enter a message.");
+    return;
+  }
 });
